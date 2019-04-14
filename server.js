51af750a164e83956f5df75a12f04bf9397191bb
Sandeep <sandy.wadhwa@gmail.com>
2019-04-15 05:21:46 +0530

    Moving VUE to JS

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static(__dirname))
+app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/test.html'))
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
